b46d633e233c9eac920ce61f7fb13b2dff298d5f
Crowdbotics <team@crowdbotics.com>
2024-02-28 09:53:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hrms-onboarding-46690",
+  url: "https://hrms-onboarding-46690.botics.co",
+  api: "https://hrms-onboarding-46690.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
